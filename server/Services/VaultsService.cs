60c0f39a6a2a1f92141ca58ad45a844dc24c12db
isaiah <isaiahpape@gmail.com>
2024-05-24 12:28:28 -0600

    backend vaults files/tables setup

---
+++ server/Services/VaultsService.cs
@@ -0,0 +1,12 @@
+namespace Keepr.Services;
+
+public class VaultsService
+{
+
+    private readonly VaultsRepository _repository;
+
+    public VaultsService(VaultsRepository repository)
+    {
+        _repository = repository;
+    }
+}
