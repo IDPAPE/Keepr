d807c0abd903b428d84088f5b5b51fb6f4f7e874
isaiah <isaiahpape@gmail.com>
2024-06-05 15:32:19 -0600

    env updated

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = 'codeworksclassroom.auth0.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
-export const audience = 'https://codeworksclassroom.com'
+export const domain = 'dev-n54qvqoaksoolgn6.us.auth0.com'
+export const clientId = 'v8F89Ap6erzudXOh5OvBHQe2HPDbXAEO'
+export const audience = 'https://isaiahpape.com'
